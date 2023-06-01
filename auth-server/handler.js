38fa99d1dddb7cb200abf317cf6b6a8423625ebd
Abbas Ali <abbasali5784@gmail.com>
2023-05-31 21:39:41 -0400

    Changed handler again and again

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -67,7 +67,7 @@ module.exports.getAccessToken = async (event) => {
         statusCode: 200,
         headers: {
           "Access-Control-Allow-Origin": "*",
-          // "Access-Control-Allow-Credentials": true,
+          "Access-Control-Allow-Credentials": true,
         },
         body: JSON.stringify(token),
       };
