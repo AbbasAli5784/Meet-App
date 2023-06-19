96a583b3de0732102c90ae3e6bf2c5ed9259558a
Abbas Ali <abbasali5784@gmail.com>
2023-06-19 14:38:32 -0400

    added pie chart

--- src/App.js
+++ src/App.js
@@ -4,12 +4,25 @@ import EventList from "./EventList";
 import CitySearch from "./CitySearch";
 import { getEvents, extractLocations } from "./api";
 import NumberOfEvents from "./NumberOfEvents";
+import {
+  ScatterChart,
+  Scatter,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+  PieChart,
+  Pie,
+  Cell,
+} from "recharts";
+import { Container, Row, Col } from "react-bootstrap";
 
 class App extends Component {
   _isMounted = false;
   state = {
     events: [],
-    allEvents: [], // New state variable to hold all events
+    allEvents: [],
     locations: [],
     numberOfEvents: 32,
   };
@@ -30,6 +43,41 @@ class App extends Component {
   componentWillUnmount() {
     this._isMounted = false;
   }
+  getData = () => {
+    const { locations, events } = this.state;
+    const data = locations.map((location) => {
+      const number = events.filter(
+        (event) => event.location === location
+      ).length;
+      const city = location.split(", ").shift();
+      return { city, number };
+    });
+    return data;
+  };
+
+  getEventTechnologies = () => {
+    const { allEvents } = this.state;
+    const terms = ["React", "JavaScript", "Angular", "Node", "Jquery"];
+    const termCounts = this.calculateTermCounts(allEvents, terms);
+
+    const data = terms.map((term) => ({ name: term, value: termCounts[term] }));
+
+    return data;
+  };
+
+  calculateTermCounts = (events, terms) => {
+    const termCount = terms.reduce((acc, term) => ({ ...acc, [term]: 0 }), {});
+
+    events.forEach((event) => {
+      terms.forEach((term) => {
+        if (event.summary.toLowerCase().includes(term.toLowerCase())) {
+          termCount[term]++;
+        }
+      });
+    });
+
+    return termCount;
+  };
 
   updateEvents = (location = "all", eventCount) => {
     const { allEvents } = this.state;
@@ -53,6 +101,7 @@ class App extends Component {
 
   render() {
     const { numberOfEvents, locations, events } = this.state;
+    const eventTechnologyData = this.getEventTechnologies();
     return (
       <div className="App">
         <CitySearch
@@ -64,6 +113,53 @@ class App extends Component {
           eventCount={numberOfEvents}
           updateEvents={this.updateEvents}
         />
+        <h4>Events in each city</h4>
+        <ResponsiveContainer height={400}>
+          <ScatterChart
+            margin={{
+              top: 20,
+              right: 20,
+              bottom: 20,
+              left: 20,
+            }}
+          >
+            <CartesianGrid />
+            <XAxis type="category" dataKey="city" name="city" />
+            <YAxis
+              allowDecimals={false}
+              type="number"
+              dataKey="number"
+              name="number of events"
+            />
+            <Tooltip cursor={{ strokeDasharray: "3 3" }} />
+            <Scatter name="" data={this.getData()} fill="#8884d8" />
+          </ScatterChart>
+        </ResponsiveContainer>
+
+        <ResponsiveContainer height={400}>
+          <PieChart width={800}>
+            <Pie
+              dataKey="value"
+              isAnimationActive={false}
+              data={eventTechnologyData}
+              cx="50%"
+              cy="50%"
+              labelLine={false}
+              label={({ name, value, percent }) =>
+                `${name}: ${(percent * 100).toFixed(0)}%`
+              }
+              outerRadius={80}
+              fill="#8884d8"
+            >
+              {eventTechnologyData.map((entry, index) => (
+                <Cell
+                  key={`cell-${index}`}
+                  // fill={COLORS[index % COLORS.length]}
+                />
+              ))}
+            </Pie>
+          </PieChart>
+        </ResponsiveContainer>
         <EventList events={events} />
       </div>
     );
